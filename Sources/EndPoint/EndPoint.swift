d6fa62e8811d9009bdf1f581a37cdbad085e84e8
Elvis Shi <3942121+el-hoshino@users.noreply.github.com>
2022-08-18 00:56:55 +0900

    Initial implement

---
+++ Sources/EndPoint/EndPoint.swift
@@ -0,0 +1,59 @@
+import Compute
+import FakeFortuneTelling
+import Foundation
+
+@main
+struct EndPoint {
+    
+    public static func main() async throws {
+        try await onIncomingRequest(router.run)
+    }
+    
+    static let router = Router()
+        .get("") { req, res in
+            try await res
+                .status(.ok)
+                .send("Hello, World!")
+        }
+        .get("/my_fortune") { req, res in
+            let result = try await askFortune(from: req)
+            try await res
+                .status(.ok)
+                .send(result)
+        }
+    
+}
+
+extension EndPoint {
+    
+    private static let jsonEncoder: JSONEncoder = {
+        let encoder = JSONEncoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        return encoder
+    }()
+    
+    static func askFortune(from request: IncomingRequest) async throws -> Data {
+        
+        let info = try await request.body.decode(ProvidedInfo.self)
+        let fortune = FortuneTeller.fortuneForYou(
+            name: info.name,
+            birthday: info.birthday,
+            bloodType: info.bloodType,
+            today: info.today
+        )
+        let encoded = try jsonEncoder.encode(fortune)
+        
+        return encoded
+        
+    }
+    
+}
+
+private struct ProvidedInfo: Decodable {
+    
+    let name: Name
+    let bloodType: BloodType
+    let birthday: YearMonthDay
+    let today: YearMonthDay
+    
+}
