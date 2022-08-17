0b87762e00cdd4b8490132c29c442a4e84fb5483
Elvis Shi <3942121+el-hoshino@users.noreply.github.com>
2022-08-18 00:24:15 +0900

    Rename target

--- Package.swift
+++ Package.swift
@@ -13,10 +13,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
-            name: "ios-junior-engineer-codecheck-backend",
+            name: "EndPoint",
             dependencies: []),
         .testTarget(
-            name: "ios-junior-engineer-codecheck-backendTests",
-            dependencies: ["ios-junior-engineer-codecheck-backend"]),
+            name: "EndPointTests",
+            dependencies: ["EndPoint"]),
     ]
 )
