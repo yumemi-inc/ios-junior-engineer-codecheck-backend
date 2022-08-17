bee93b4d1f8ffeb50f93831aec62d00f57de7447
Elvis Shi <3942121+el-hoshino@users.noreply.github.com>
2022-08-18 00:30:22 +0900

    Add Compute runtime

--- Package.swift
+++ Package.swift
@@ -5,16 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "ios-junior-engineer-codecheck-backend",
+    platforms: [.macOS(.v10_15)],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url: "https://github.com/swift-cloud/Compute",
+            from: "1.0.0"
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "EndPoint",
-            dependencies: []),
+            dependencies: [
+                "Compute",
+            ]),
         .testTarget(
             name: "EndPointTests",
             dependencies: ["EndPoint"]),
