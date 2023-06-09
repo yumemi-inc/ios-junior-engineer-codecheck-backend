0b14f0778f6925edf575fea8725ca257c2093eee
Elvis Shi <3942121+el-hoshino@users.noreply.github.com>
2023-06-09 15:24:42 +0900

    Revert "chore: bump Compute"

--- Package.swift
+++ Package.swift
@@ -1,17 +1,17 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ios-junior-engineer-codecheck-backend",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v10_15)],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(
             url: "https://github.com/swift-cloud/Compute",
-            from: "2.17.0"
+            from: "1.11.0"
         ),
 //        .package(
 //            url: "https://github.com/johnsundell/ink.git",
--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # ios-junior-engineer-codecheck-backend
 
 iOS未経験者エンジニア向けの採用コードチェックに使うバックエンドです。詳細は[デプロイ先](https://yumemi-ios-junior-engineer-codecheck.app.swift.cloud)をご覧ください。
-
-## warning
-
-PRのbranch名は6文字以内にしてください
