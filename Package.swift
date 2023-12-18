6fbf3b8ccf14912f8e28be1dfaf3f6fccf553eda
novr (Nobuhisa Komiya) <n_komiya@yumemi.co.jp>
2023-12-18 11:26:11 +0900

    update: swift-openapi v1.1.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -21,9 +21,9 @@ let package = Package(
             url: "https://github.com/yumemi-inc/fake-fortune-telling",
             from: "0.2.5"
         ),
-        .package(url: "https://github.com/apple/swift-openapi-generator", from: "0.3.4"),
-        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "0.3.6"),
-        .package(url: "https://github.com/novr/swift-openapi-compute", from: "0.1.1"),
+        .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.1.0"),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.1.0"),
+        .package(url: "https://github.com/novr/swift-openapi-compute", from: "0.1.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
